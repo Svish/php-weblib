63124607e869109aa26a30ce8fbda71e0bd2d523
Torleif Berger <torleif@outlook.com>
2017-08-01 02:33:18 +0200

    Added File::append

--- src/File.php
+++ src/File.php
@@ -1,21 +1,31 @@
 <?php
 
+use Error\Oops;
+
 /**
  * File helper.
+ * 
+ *  - Uses flock to (hopefully) prevent any issues with
+ *      potentially simultaneous writes.
  *
  * @see http://php.net/manual/en/function.fopen.php
  * @see http://php.net/manual/en/function.flock.php
  */
 class File
 {
-	public static function get(string $filename, $default = NULL)
+	/**
+	 * Gets data from $file.
+	 * 
+	 * @return mixed Returns data if files exists; otherwise $default.
+	 */
+	public static function get(string $file, $default = NULL)
 	{
-		if( ! file_exists($filename))
+		if( ! file_exists($file))
 			return $default;
 		
-		$fp = fopen($filename, 'r');
+		$fp = fopen($file, 'r');
 		flock($fp, LOCK_SH);
-		$contents = fread($fp, filesize($filename));
+		$contents = fread($fp, filesize($file));
 		flock($fp, LOCK_UN);
 		fclose($fp);
 
@@ -23,32 +33,67 @@ class File
 	}
 
 
-
-	public static function put(string $filename, $contents)
+	/**
+	 * Puts $data in $file.
+	 * 
+	 * @return string Returns $data for chaining.
+	 */
+	public static function put(string $file, string $data): string
 	{
-		if(empty($filename))
-			throw new Exception(__METHOD__.' called with empty filename.');
+		if(empty($file))
+			throw new Oops(__METHOD__.' called with empty file.');
 
-		self::mkdir(dirname($filename));
+		self::mkdir(dirname($file));
 
-		$fp = fopen($filename, 'c');
+		$fp = fopen($file, 'c');
 		flock($fp, LOCK_EX);
 		ftruncate($fp, 0);
-		fwrite($fp, $contents);
+		fwrite($fp, $data);
 		fflush($fp);
 		flock($fp, LOCK_UN);
 		fclose($fp);
 
-		return $contents;
+		return $data;
+	}
+
+	/**
+	 * Appends $data to $file.
+	 * 
+	 * Uses $prefix unless empty file.
+	 * 
+	 * @return string Returns $data for chaining.
+	 */
+	public static function append(string $file, string $data, string $prefix = "\r\n"): string
+	{
+		if(empty($file))
+			throw new Oops(__METHOD__.' called with empty file.');
+
+		self::mkdir(dirname($file));
+
+		$fp = fopen($file, 'a');
+		flock($fp, LOCK_EX);
+		if(fstat($fp)['size'] > 0)
+			fwrite($fp, $prefix);
+		fwrite($fp, $data);
+		fflush($fp);
+		flock($fp, LOCK_UN);
+		fclose($fp);
+
+		return $data;
 	}
 
 
 
-	public static function mkdir(string $dir)
+	/**
+	 * Creates $dir if not existing, otherwise does nothing.
+	 * 
+	 * @return string Returns $dir for chaining
+	 * @see https://en.wikipedia.org/wiki/Chmod#System_call
+	 */
+	public static function mkdir(string $dir): string
 	{
 		if( ! is_dir($dir))
 		{
-			// https://en.wikipedia.org/wiki/Chmod#System_call
 			@mkdir($dir, 06750, true);
 			@chmod($dir, 06750);
 		}
@@ -57,12 +102,19 @@ class File
 
 
 
-	public static function rdelete(string $directory, bool $keep_self = false)
+
+	/**
+	 * Recursively deletes $directory.
+	 * 
+	 * @param string $dir Directory to delete
+	 * @param bool|bool $keep_self If false, $dir will also be deleted.
+	 */
+	public static function rdelete(string $dir, bool $keep_self = false): void
 	{
-		if( ! is_dir($directory))
+		if( ! is_dir($dir))
 			return;
 
-		$it = new RecursiveDirectoryIterator($directory,FilesystemIterator::SKIP_DOTS);
+		$it = new RecursiveDirectoryIterator($dir,FilesystemIterator::SKIP_DOTS);
 		$it = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
  
 		foreach($it as $file)
@@ -72,6 +124,6 @@ class File
 				@unlink($file->getRealPath());
 
 		if( ! $keep_self)
-			@rmdir($directory);
+			@rmdir($dir);
 	}
 }
